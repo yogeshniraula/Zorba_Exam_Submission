e14dfe311cd76a1c27f9b2d469d6178c2d0df4b3
yoges <yogesh_niraula@icloud.com>
2024-08-04 20:04:55 -0500

    Committed for 0804

---
+++ src/main/java/xmlExec/writingXmlBased.java
@@ -0,0 +1,9 @@
+package xmlExec;
+import entities0804.Teacher;
+import entities0804.Student;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+public class writingXmlBased {
+}
